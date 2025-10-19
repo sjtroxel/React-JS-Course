a2b07271db1515e8dc879708659ee366c8ad2fce
sjtroxel <sjtroxel@protonmail.com>
2025-10-18 21:02:16 -0500

    react components and javascript review thru 51:30

---
+++ 01-static-pages/first-react/index-0.jsx
@@ -0,0 +1,12 @@
+// import { createElement } from "react"
+import { createRoot } from "react-dom/client"
+
+const root = createRoot(document.getElementById("root"))
+const reactElement = <h1>Hello from JSX!</h1>
+// const reactElement = createElement("h1", null, "Hello from createElement!")
+
+console.log(reactElement)
+root.render(
+    reactElement
+)
+
