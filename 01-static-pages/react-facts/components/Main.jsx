6627e8c9e64630feaf75c439ab8a42b0cff64d6e
sjtroxel <sjtroxel@protonmail.com>
2025-10-27 11:12:34 -0500

    finish react-facts section 01 and setup travel-journal, thru 2:20.33

--- 01-static-pages/react-facts/components/Main.jsx
+++ 01-static-pages/react-facts/components/Main.jsx
@@ -1,5 +1,23 @@
+/**
+ * Challenge: Build out the main content section!
+ * 
+ * For now, skip 2 aspects of the design and we'll work on these later:
+ *  1. the colored bullets in the list
+ *  2. the larger gray React logo on the side
+ */
+
+
 export default function Main() {
    return ( 
-   <h1>Main component goes here!</h1>
+   <main>
+    <h1>Fun facts about React!</h1>
+    <ul className="facts-list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 200K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+   </main>
    )
 }
--- 01-static-pages/react-facts/components/Navbar.jsx
+++ 01-static-pages/react-facts/components/Navbar.jsx
@@ -1,5 +1,21 @@
+/**
+ * Challenge: complete the Navbar to match the Figma design
+ * 
+ * Hints:
+ * - for semantic HTML purposes, the Navbar should render
+ *   a <header> with a <nav> nested inside. The image and "ReactFacts"
+ *   text elements can both be rendered as children inside the <nav>
+ * - reference the Figma design for the most accurate info about
+ *   colors, sizes, font information, etc.
+ */
+
 export default function Navbar() {
     return (
-        <h1>Navbar component goes here!</h1>
+        <header>
+            <nav>
+                <img src="images/react-logo.png" alt="React logo" />
+                <span>React Facts!</span>
+            </nav>
+        </header>
     )
 }
