f62af65b112dcd3db346af25f97de927dbd0f599
sjtroxel <sjtroxel@protonmail.com>
2025-10-30 12:56:08 -0500

    four Strawberry 😻components in Contact.jsx (thru 2:56.14) woohoo

---
+++ 02-data-driven-react/contact-cards/components/Contact.jsx
@@ -0,0 +1,27 @@
+export default function Contact() {
+    return (
+        <>
+         <article className="contact-card">
+                <img 
+                    src="./images/strawberry.png"
+                    alt="Photo of Strawberry"
+                />
+                <h3>Strawberry</h3>
+                <div className="info-group">
+                    <img 
+                        src="./images/phone-icon.png" 
+                        alt="phone icon" 
+                    />
+                    <p>(573) GOOD-CAT</p>
+                </div>
+                <div className="info-group">
+                    <img 
+                        src="./images/mail-icon.png" 
+                        alt="mail icon"
+                    />
+                    <p>strawberry@goodgirl.cat</p>
+                </div>
+            </article>
+        </>
+    )
+}
