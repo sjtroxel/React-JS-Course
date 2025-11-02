02eaf383e9b2a96957dd54b46f5df9fe128c72d7
sjtroxel <sjtroxel@protonmail.com>
2025-11-02 15:47:55 -0600

    pass obj. as props & sprd ...oper./end chapter 2 (thru 4:32.30)

--- 02-data-driven-react/travel-journal/App.jsx
+++ 02-data-driven-react/travel-journal/App.jsx
@@ -2,78 +2,36 @@ import Header from "./components/Header"
 import Entry from "./components/Entry"
 import data from "./data"
 
-/**
- * Challenge: 
- * - import the array of data from data.js
- * - map over the array to create an <Entry /> component
- *   for every item in the data array.
- * - display the array of Entry components in place of the 
- *   current hard-coded <Entry /> instance.
- */
-
-
-    // BOB:
 export default function App() {
-     const entryElements = data.map((entry) => {
+    
+    const entryElements = data.map((entry) => {
         return (
-            <Entry 
-                img= {{
-                    src: entry.img.src,
-                    alt: entry.img.alt
-                }}
-                title= {entry.title}
-                country= {entry.country}
-                googleMapsLink = {entry.googleMapsLink}
-                dates = {entry.dates}
-                text = {entry.text}
+            // <Entry
+            //     key={entry.id}
+            //     img={entry.img}
+            //     title={entry.title}
+            //     country={entry.country}
+            //     googleMapsLink={entry.googleMapsLink}
+            //     dates={entry.dates}
+            //     text={entry.text}
+            // />
+            <Entry
+                key={entry.id}  // key has to stay - this is specific to React
+                {...entry}      // this works but it can be confusing - *too* simplistic
             />
+            // <Entry
+            //     key={entry.id}  // key has to stay - this is specific to React
+            //     entry={entry}   // but the other props can all be replaced with {entry}
+            // />
         )
-     })
-        return (
+    })
+    
+    return (
         <>
-        <Header />
+            <Header />
             <main className="container">
                 {entryElements}
             </main>
-        </> )
+        </>
+    )
 }
-
-    // ME w/ CHATGPT
-// export default function App() {
-//      const entryElements = data.map((entry) => {
-//             // return <Entry key={entry.id} {...entry} />
-//         return <Entry key={entry.id}
-//                 img={entry.img}
-//                 title={entry.title}
-//                 country={entry.country}
-//                 googleMapsLink={entry.googleMapsLink}
-//                 dates={entry.dates}
-//                 text={entry.text} />
-//         })
-//         return (
-//         <>
-//         <Header />
-//             <main className="container">
-//                 {entryElements}
-//             </main>
-//         </> )
-// }
-
-    // OLD:
-// export default function App() {
-//     return (
-//         <>
-//         <Header />
-//             <main className="container">
-//         <Entry 
-//             img={{ 
-//                 src: "https://scrimba.com/links/travel-journal-japan-image-url", 
-//                 alt:"Mount Fuji" }}
-//             title="Mount Fuji"
-//             country="Japan"
-//             googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-//             dates="12 Jan, 2021 - 24 Jan, 2021"
-//             text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists." />
-//             </main>
-//         </> )
-// }
--- 02-data-driven-react/travel-journal/components/Entry.jsx
+++ 02-data-driven-react/travel-journal/components/Entry.jsx
@@ -1,3 +1,26 @@
+/**
+ * Challenge: Fix our component! 😱
+ */
+
+// export default function Entry(props) {
+//     console.log(props)
+//     return ( 
+//     <article className="journal-entry">
+//         <div className="main-image-container">
+//     <img src={props.entry.img.src} alt={`Photo of ${props.entry.img.alt}`} className="main-image"/>
+//         </div>
+//         <div className="info-container">
+//        <img src="../images/marker.png" alt="map marker icon" className="marker"/>
+//        <span className="country">{props.entry.country}</span>
+//        <a href={props.entry.googleMapsLink}>View on Google Maps!</a>
+//        <h2 className="entry-title">{props.entry.title}</h2>
+//        <p className="trip-dates">{props.entry.dates}</p>
+//        <p className="entry-text">{props.entry.text}</p>
+//         </div>
+        
+//     </article> )
+// }
+
 export default function Entry(props) {
     console.log(props)
     return ( 
