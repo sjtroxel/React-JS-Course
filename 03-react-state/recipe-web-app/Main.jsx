93146ddcf83b57714a110645b53156cdd22f8d81
sjtroxel <sjtroxel@protonmail.com>
2025-11-04 17:44:08 -0600

    build and style add-ingredient form (thru 4:55.39)

--- 03-react-state/recipe-web-app/Main.jsx
+++ 03-react-state/recipe-web-app/Main.jsx
@@ -1,7 +1,13 @@
 export default function Main() {
     return (
-    <>
-    <p>Chef Claude</p>
-    </>
+    <main>
+        <form className="add-ingredient-form">
+            <input 
+                type="text"
+                placeholder="e.g. oregano"
+                aria-label="Add Ingredient" />
+            <button>Add Ingredient!</button>
+        </form>
+    </main>
     )
 }
