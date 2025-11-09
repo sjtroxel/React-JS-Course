c1c5331e5015a90d8070f5357cf59444c1ba0104
sjtroxel <sjtroxel@protonmail.com>
2025-11-09 14:01:45 -0600

    updating arrays with useState (thru 6:10.58)

--- 03-react-state/ternary-practice-toggling-state/index.jsx
+++ 03-react-state/ternary-practice-toggling-state/index.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import App1 from './App-1';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App1 />)
