82cbb8571607ddb40f4554ee90cf5ec7fcb5c7c8
sjtroxel <sjtroxel@protonmail.com>
2025-11-13 15:23:37 -0600

    forms: checkboxes / select&option / Object.fromEntries (thru 7:17.10)

--- 03-react-state/react-forms-continued/index.jsx
+++ 03-react-state/react-forms-continued/index.jsx
@@ -4,20 +4,34 @@ import ReactDOM from 'react-dom/client';
 function App() {
 
     /**
-     * Challenge: grab the employment status from the form and log it to
-     * the console. (Remember to select one of the radios before submitting!)
-     * 
-     * Note: This won't work the way you might expect, quite yet!
+     * Challenge: see if you can remember how to grab an array of checked
+     * items from the dietaryRestrictions checkboxes.
      */
 
 
   function signUp(formData) {
-    const email = formData.get("email")
-    const password = formData.get("password")
-    const employmentStatus = formData.get("employmentStatus")
-    console.log(password)
-    console.log(email)
-    console.log(employmentStatus)
+    // console.log(Object.fromEntries(formData))   
+            // ^^ but selecting multiple-checkboxes (dietaryRestrictions) still returns only one
+    
+            // so we have to do this v v
+        const data = Object.fromEntries(formData)
+        const dietaryData = formData.getAll("dietaryRestrictions")
+        console.log(data)       // shows only gluten-free
+        const allData = {
+            ...data, dietaryRestrictions: dietaryData
+        }
+        console.log(allData)    // shows all the dietaryRestrictions boxes I checked
+
+    // const email = formData.get("email")
+    // const password = formData.get("password")
+    // const employmentStatus = formData.get("employmentStatus")
+    // const dietaryRestrictions = formData.getAll("dietaryRestrictions")
+    // const favColor = formData.get("favColor")
+    // console.log(password)
+    // console.log(email)
+    // console.log(employmentStatus)
+    // console.log(dietaryRestrictions)
+    // console.log(favColor)
   }
 
   return (
@@ -50,6 +64,34 @@ function App() {
         </label>
         </fieldset>
 
+        <fieldset>
+            <legend>Dietary Restrictions:</legend>
+        <label>
+            <input type="checkbox" name="dietaryRestrictions" value="kosher" />
+            Kosher
+        </label>
+        <label>
+            <input type="checkbox" name="dietaryRestrictions" value="vegan" />
+            Vegan
+        </label>
+        <label>
+            <input type="checkbox" name="dietaryRestrictions" value="gluten-free" defaultChecked={true} />
+            Gluten-Free
+        </label>
+        </fieldset>
+
+        <label htmlFor="favColor">What is your favorite color?</label>
+        <select id="favColor" name="favColor" defaultValue="" required>
+            <option value="" disabled>-- Choose a color! --</option>
+            <option value="red">Red</option>
+            <option value="orange">Orange</option>
+            <option value="yellow">Yellow</option>
+            <option value="green">Green</option>
+            <option value="blue">Blue</option>
+            <option value="indigo">Indigo</option>
+            <option value="violet">Violet</option>
+        </select>
+
         <button>Submit</button>
 
       </form>
@@ -60,3 +102,16 @@ function App() {
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
 export default App;
+
+/**
+ * {
+  "email": "joe@schmoe.com",
+  "password": "password123",
+  "description": "This is a description.",
+  "employmentStatus": "unemployed",
+  "dietaryRestrictions": "gluten-free",
+  "favColor": "violet"
+}
+ * Object { email: "joe@schmoe.com", password: "password123", description: "This is a description.", employmentStatus: "unemployed", dietaryRestrictions: "gluten-free", favColor: "violet" }
+index.jsx:15:13
+ */
