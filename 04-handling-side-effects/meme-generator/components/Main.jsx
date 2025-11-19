f54198b8a19646f8f3ada0968d76fe93499969ac
sjtroxel <sjtroxel@protonmail.com>
2025-11-19 17:45:13 -0600

    setup meme generator and refactor meme into React state (thru 9:59.15)

---
+++ 04-handling-side-effects/meme-generator/components/Main.jsx
@@ -0,0 +1,45 @@
+import {useState} from "react"
+
+export default function Main() {
+    /**
+     * Challenge: move the hard-coded meme info into React state. Use
+     * an object with `topText`, `bottomText`, and `imageUrl` properties
+     * and set the initial values to the ones hardcoded below.
+     */
+
+    const [meme, setMeme] = useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    console.log(setMeme)
+
+    return (
+        <main>
+            <div className="form">
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                    />
+                </label>
+
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                    />
+                </label>
+                <button>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={meme.imageUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
+            </div>
+        </main>
+    )
+}
