bb494cf9dc03f46710b6de3a40087535785e5759
sjtroxel <sjtroxel@protonmail.com>
2025-11-23 08:41:42 -0600

    introduce useRef into Chef Claude app (thru 11:29.45)

--- 03-react-state/recipe-web-app/components/Main.jsx
+++ 03-react-state/recipe-web-app/components/Main.jsx
@@ -1,29 +1,15 @@
 import React from "react"
-import IngredientsList from "./components/IngredientsList"
-import ClaudeRecipe from "./components/ClaudeRecipe"
-import { getRecipeFromChefClaude } from "./ai"
+import IngredientsList from "./IngredientsList"
+import ClaudeRecipe from "./ClaudeRecipe"
+import { getRecipeFromChefClaude } from "../ai"
 // import { getRecipeFromMistral } from "./ai"
 
-/**
- * Challenge: Get a recipe from the AI!
- * 
- * This will be a bit harder of a challenge that will require you
- * to think critically and synthesize the skills you've been
- * learning and practicing up to this point.
- * 
- * Using either the `getRecipeFromChefClaude` function or the
- * `getRecipeFromMistral` function, make it so that when the user
- * clicks "Get a recipe!", the test response from the AI is displayed
- * in the <ClaudeRecipe> component.
- * 
- * For now, just have it render the raw markdown that the AI returns;
- * don't worry about making it look nice yet. (We're going to use a 
- * package that will render the markdown for us soon.)
- */
 
 export default function Main() {
     const [ingredients, setIngredients] = React.useState([])
     const [recipe, setRecipe] = React.useState("")
+    const recipeSection = React.useRef(null)
+    console.log(recipeSection)
     
     async function getRecipe() {
         const recipeMarkdown = await getRecipeFromChefClaude(ingredients)
@@ -55,6 +41,7 @@ export default function Main() {
             
             {ingredients.length > 0 && (
                 <IngredientsList 
+                    ref={recipeSection}
                     ingredients={ingredients}
                     getRecipe={getRecipe}
                     deleteIngredient={deleteIngredient} />
