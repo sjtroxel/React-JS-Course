e88a8caae27e1b20a3789e9253e5adf8d31e3e7c
sjtroxel <sjtroxel@protonmail.com>
2025-11-26 14:49:57 -0600

    setup/style status, language-chips w/ language.js array, word-display (thru 13:37.22)

--- 06-assembly-endgame-project/App.jsx
+++ 06-assembly-endgame-project/App.jsx
@@ -1,58 +1,76 @@
 import React from "react"
-
-
-/**
- * Project planning:
- * 
- * Questions to ask yourself before writing any code:
- * 
- * - What are the main containers of elements I need
- *   in this app?
- * 
- *      Header
- *      Status Bar/Section  
- *      List of Languages
- *      Word with Blank Letter Spots*      Keys with Clickable Letters
- *      New Game Button
- * 
- * - What values will need to be saved in state vs.
- *   what values can be derived from the state?
- * 
- *      Is the game over or not?
- *      Has the user won or lost?
- *      How many wrong guesses has the user had?
- *      What letters have already been guessed?
- *      What word is the user trying to guess?
- *      Etc.
- * 
- * - How will the user interact with the app? What
- *   events do I need to handle?
- * 
- *      The user will be clicking on the letter keys
- *      which will cause re-renders, i.e. fill in 
- *      missing letters, destroy languages, etc.
- *      Also there is the new game button, which will
- *      only show up when the game is over. 
- */
+import { languages } from "./languages"
 
 
 /**
  * Goal: Build out the main parts of our app!
  * 
- * Challenge: Add a header, with the game title and
- * description. Starting out easy!!
+ * Challenge: Build a status section below the header.
+ * For now, you can just hard-code in the styles for a
+ * winning game, and we'll make it more dynamic later.
+ * 
+ * Challenge: Create the language chips. Use the 
+ * `languages.js` file to pull in the array of languages
+ * to use, which contains the language name, background
+ * color, and text color:
+ * 
+ *      Hint for layout: use a flex container that can
+ *      wrap to layout the languages.
+ * 
+ * Challenge: 
+ * 1. Save a "currentWord" in state. Initialize as "react".
+ * 2. Map over the letters of the word (you'll need to turn 
+ *    the string into an array of letters first) and display
+ *    each one as a <span>. Capitalize the letters when
+ *    displaying them.
+ * 3. Style to look like the design. You can get the underline 
+ *    effect on the box using `border-bottom`.
  */
+ 
 
 
 export default function AssemblyEndgame() {
+    const [currentWord, setCurrentWord] = React.useState("react")
+    console.log(setCurrentWord)
+
+    const languageElements = languages.map(lang => {
+        const styles = {
+            backgroundColor: lang.backgroundColor,
+            color: lang.color
+        }
+        return (
+        <span key={lang.name} className="chip" style={styles}>
+            {lang.name}
+        </span>
+    )})
+
+    const letterElements = currentWord.split("").map(
+        (letter, index) => (
+            <span key={index} className="letter">
+                {letter.toUpperCase()}
+            </span>
+        )
+    )
+
+
     return (
         <main>
             <header>
                 <h1>Assembly: Endgame</h1>
-                <p>Guess the word within 8 attempts to keep the programming
-                    world safe from Assembly!!
+                <p>Guess the word within 8 attempts to keep the
+                    programming world safe from Assembly!!
                 </p>
             </header>
+            <section className="game-status">
+                <h2>You win!</h2>
+                <p>Well done! 🍕</p>
+            </section>
+            <section className="language-chips">
+                {languageElements}
+            </section>
+             <section className="word">
+                {letterElements}
+            </section>
         </main>
     )
 }
